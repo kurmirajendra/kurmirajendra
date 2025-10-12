aafe389bebf08e5ee47ab8ed6c2c0ffc8f061396
Rajendra <rajendrakurmi78@gmail.com>
2025-10-12 22:48:08 +0530

    Add files via upload

---
+++ SumOfArrayAverage.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {-3, -2, 0, -7, 25, 35, -12, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0, count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+            count++;
+        }
+    }
+
+    if (count > 0) {
+        double average = (double)sum / count;
+        cout << "Average of positive numbers is " << average << endl;
+    } else {
+        cout << "No positive numbers in the array." << endl;
+    }
+
+    return 0;
+}
+
+
